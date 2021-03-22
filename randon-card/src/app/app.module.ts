7ad6200373674b98b6c28a8cdc48f3e1d265fae9
Alabhya268 <singhalabhya32@gmail.com>
2021-03-22 14:53:46 +0530

    Initial commit

---
+++ randon-card/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ todo-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { AppComponent } from './app.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { HeaderComponent } from './layout/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoFormComponent,
+    TodosComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
